327488d8f9ee173dbb7140cd31b34fc252ac4222
Martin Kruitz <info@kruitzit.nl>
2016-05-19 22:53:48 +0200

    Refactored NeighbourGetter

--- src/NeighbourGetter.js
+++ src/NeighbourGetter.js
@@ -1,14 +1,30 @@
 NeighbourGetter = function(population) {
+    const NO_NEIGHBOUR = -1;
+    
     this.getNeighboursFor = function(x) {
         var results = [];
         if (typeof x !== 'undefined') {
-            if(x > 0) {
-                results.push(population[x-1]);
-            }
-            if(population.length > x+1) {
-                results.push(population[x+1]);
-            }
+            push(results, leftNeighbour(x));
+            push(results, rightNeighbour(x));
         }
         return results;
-    }
+    };
+
+    var leftNeighbour = function(x) {
+        var left = x-1;
+        return left >= 0
+            ? left
+            : NO_NEIGHBOUR;
+    };
+    var rightNeighbour = function(x) {
+        var right = x+1;
+        return population.length > right
+            ? right
+            : NO_NEIGHBOUR;
+    };
+    var push = function(results, coord) {
+        if(coord != NO_NEIGHBOUR) {
+            results.push(population[coord]);
+        }
+    };
 };
