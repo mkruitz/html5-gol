e7b4ae8ea9d808fe791ce748e74a3a854e795ae7
Martin Kruitz <info@kruitzit.nl>
2016-05-26 21:56:22 +0200

    Reverted, refactor commit "Refactored NeighbourGetter" with sha 327488d8f9ee173dbb7140cd31b34fc252ac4222

--- src/NeighbourGetter.js
+++ src/NeighbourGetter.js
@@ -1,36 +1,18 @@
 NeighbourGetter = function(population) {
-    const NO_NEIGHBOUR = -1;
-
     this.getNeighboursFor = function(x, y) {
         var results = [];
         var dimension = population;
-
         if (typeof y !== 'undefined') {
             dimension = dimension[0];
         }
         if (typeof x !== 'undefined') {
-            push(results, dimension, leftNeighbour(x));
-            push(results, dimension, rightNeighbour(x, dimension));
+            if(x > 0) {
+                results.push(dimension[x-1]);
+            }
+            if(dimension.length > x+1) {
+                results.push(dimension[x+1]);
+            }
         }
-
         return results;
-    };
-
-    var leftNeighbour = function(x) {
-        var left = x-1;
-        return left >= 0
-            ? left
-            : NO_NEIGHBOUR;
-    };
-    var rightNeighbour = function(x, dimension) {
-        var right = x+1;
-        return dimension.length > right
-            ? right
-            : NO_NEIGHBOUR;
-    };
-    var push = function(results, dimension, coord) {
-        if(coord !== NO_NEIGHBOUR) {
-            results.push(dimension[coord]);
-        }
-    };
+    }
 };
