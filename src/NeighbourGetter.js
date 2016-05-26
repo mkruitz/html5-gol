871d9324ff20d3ebac4bc9d460b9639e1d6f9453
Martin Kruitz <info@kruitzit.nl>
2016-05-26 22:19:29 +0200

    Refactor: explained magical numbers -> neighbour position ( left and right )

--- src/NeighbourGetter.js
+++ src/NeighbourGetter.js
@@ -2,15 +2,19 @@ NeighbourGetter = function(population) {
     this.getNeighboursFor = function(x, y) {
         var results = [];
         var dimension = population;
+
         if (typeof y !== 'undefined') {
             dimension = dimension[0];
         }
         if (typeof x !== 'undefined') {
-            if(0 <= x-1) {
-                results.push(dimension[x-1]);
+            var left = x-1;
+            var right = x+1;
+
+            if(0 <= left) {
+                results.push(dimension[left]);
             }
-            if(dimension.length > x+1) {
-                results.push(dimension[x+1]);
+            if(dimension.length > right) {
+                results.push(dimension[right]);
             }
         }
         return results;
