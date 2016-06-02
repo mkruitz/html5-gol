8075fced2296e4f8f4adc3a924c8e89b31cd9fd2
Martin Kruitz <info@kruitzit.nl>
2016-06-02 19:35:17 +0200

    Converted code + test into ES6 code

--- src/NeighbourGetter.js
+++ src/NeighbourGetter.js
@@ -1,36 +1,41 @@
-NeighbourGetter = function(population) {
-    const NO_NEIGHBOUR = -1;
+export default class NeighbourGetter {
+    constructor(newPopulation) {
+        this.population = newPopulation;
 
-    this.getNeighboursFor = function(x, y) {
+        this.NO_NEIGHBOUR = -1;
+    }
+
+
+    getNeighboursFor(x, y) {
         var results = [];
-        var dimension = population;
+        var dimension = this.population;
 
         if (typeof y !== 'undefined') {
             dimension = dimension[0];
         }
         if (typeof x !== 'undefined') {
-            push(results, dimension, leftNeighbour(x));
-            push(results, dimension, rightNeighbour(x, dimension));
+            this.push(results, dimension, this.leftNeighbour(x));
+            this.push(results, dimension, this.rightNeighbour(x, dimension));
         }
 
         return results;
-    };
+    }
 
-    var leftNeighbour = function(x) {
+    leftNeighbour(x) {
         var left = x-1;
         return left >= 0
             ? left
-            : NO_NEIGHBOUR;
-    };
-    var rightNeighbour = function(x, dimension) {
+            : this.NO_NEIGHBOUR;
+    }
+    rightNeighbour(x, dimension) {
         var right = x+1;
         return dimension.length > right
             ? right
-            : NO_NEIGHBOUR;
-    };
-    var push = function(results, dimension, coord) {
-        if(coord !== NO_NEIGHBOUR) {
+            : this.NO_NEIGHBOUR;
+    }
+    push(results, dimension, coord) {
+        if(coord !== this.NO_NEIGHBOUR) {
             results.push(dimension[coord]);
         }
-    };
-};
+    }
+}
