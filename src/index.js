b7e064eb46f6fa26ba9a5c390593b8d672106af4
Martin Kruitz <info@kruitzit.nl>
2016-06-11 12:54:52 +0200

    Added express to listen on a port and output 'hello app'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello app');
+var express = require('express');
+var app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.send('hello app');
+});
+
+app.listen(port, function () {
+    console.log('Listening on port:', port);
+});
